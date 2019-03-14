7fb7312091dbad1e108f09ddb09c0b67f7780642
Tucker Boniface <tucker@boniface.tech>
2019-03-14 15:21:58 -0700

    add mask thing to make it easier to find which values wwere inputted

--- solver.c
+++ solver.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+#define IN_MASK(m, o) (m & (int)pow(2, o))
+
+#define INPUT_SIZE 16
 #define NUM_OPTS 6
 #define OPTS_STR "A", "a", "B", "b", "C", "c"
 
 enum opts{A, a, B, b, C, c};
 
 int main(void) {
+    // TODO: maybe use a struct for this so I don't do weird stuff with NULL
     double *vals[NUM_OPTS];
     const char *opts_str[NUM_OPTS] = {OPTS_STR};
+    int opts_mask = 0;
+    char input[INPUT_SIZE];
 
     for (int i = 0; i < NUM_OPTS; i++) {
         vals[i] = malloc(sizeof(double));
@@ -21,11 +28,15 @@ int main(void) {
 
     for (int i = 0; i < NUM_OPTS; i++) {
         printf("%s: ", opts_str[i]);
-        scanf("%lf", vals[i]);
+        fgets(input, INPUT_SIZE, stdin);
+        if (sscanf(input, " %lf", vals[i]) > 0) {
+            opts_mask |= (int)pow(2, i);
+        }
         printf("\n");
     }
 
-    printf("A: %lf, a: %lf, B: %lf, b: %lf, C: %lf, c: %lf\n", *vals[A], *vals[a], *vals[B], *vals[b], *vals[C], *vals[c]);
+    printf("mask: %x\nA: %lf, a: %lf, B: %lf, b: %lf, C: %lf, c: %lf\n", opts_mask, *vals[A], *vals[a], *vals[B], *vals[b], *vals[C], *vals[c]);
+    printf("A: %s, a: %s, B: %s, b: %s, C: %s, c: %s\n", IN_MASK(opts_mask, A) ? "yeek":"nope", IN_MASK(opts_mask, a) ? "yeek":"nope", IN_MASK(opts_mask, B) ? "yeek":"nope", IN_MASK(opts_mask, b) ? "yeek":"nope", IN_MASK(opts_mask, C) ? "yeek":"nope", IN_MASK(opts_mask, c) ? "yeek":"nope");
 
     return EXIT_SUCCESS;
 }
